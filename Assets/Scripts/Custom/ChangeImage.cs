b3baec52b4804be8ec069c9cfef70f9049751e7f
Shinmiken <zxcvbnmsib@gmail.com>
2025-05-05 16:42:13 +0900

    int : 캐릭터 이미지 변경 구현완료
    캐릭터 이미지 변경 구현완료

--- Assets/Scripts/Custom/ChangeImage.cs
+++ Assets/Scripts/Custom/ChangeImage.cs
@@ -15,12 +15,29 @@ public class ChangeImage : MonoBehaviour
 
     public AnimatorOverrideController[] skinOverrides; // 스킨별 컨트롤러 배열
 
-    public void ChangeSkin(int skinIndex)
+    public void ChangeSkin()
     {
-        animator = player.GetComponent<Animator>();
-        if (skinIndex < skinOverrides.Length)
+        Image image = GetComponent<Image>();
+        string str = image.sprite.name;
+        int i = 0;
+        for (i = 0; i < skinOverrides.Length; i++)
         {
-            animator.runtimeAnimatorController = skinOverrides[skinIndex];
+            if(str.Contains(i.ToString())) // 이미지 번호가 포함되어 있으면 그 번호를 부여
+            {
+                break;
+            }
+            else if (str.Contains("ten"))
+            {
+                i = 10;
+                break;
+            }
+            else if(str.Contains("elev"))
+            {
+                i = 11;
+                break;
+            }
         }
+        animator = player.GetComponent<Animator>();
+        animator.runtimeAnimatorController = skinOverrides[i];
     }
 }
